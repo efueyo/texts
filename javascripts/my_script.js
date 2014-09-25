922417fabb52823cf15eef216815b21968d4fc78
efueyo <enrique.fueyo@gmail.com>
2014-09-24 19:50:29 -0500

    Javascript refactored + regexp substitution

--- javascripts/my_script.js
+++ javascripts/my_script.js
@@ -1,13 +1,20 @@
-window.upperCase = function(elem){
+window.getTextArea = function(){
+  return $('#text-pool');
+}
+
+window.upperCase = function(){
+  var elem = getTextArea();
   var text = elem.val();
   elem.val(text.toUpperCase());
 }
-window.lowerCase = function(elem){
+window.lowerCase = function(){
+  var elem = getTextArea();
   var text = elem.val();
   elem.val(text.toLowerCase());
 }
 
-window.capitalize = function(elem){
+window.capitalize = function(){
+  var elem = getTextArea();
   var regex = /^[\u00C0-\u1FFF\u2C00-\uD7FF\w]|\s[\u00C0-\u1FFF\u2C00-\uD7FF\w]/g;
   var text = elem.val();
   text = text.toLowerCase().replace(regex, function(letter) {
@@ -16,7 +23,8 @@ window.capitalize = function(elem){
   elem.val(text);
 }
 
-window.random = function(elem){
+window.random = function(){
+  var elem = getTextArea();
   var text = elem.val().toLowerCase();
   var new_text = "";
   for(var i=0;i<text.length;i++){
@@ -28,3 +36,16 @@ window.random = function(elem){
   }
   elem.val(new_text); 
 }
+
+window.regex = function(src,dest){
+  var elem = getTextArea();
+  var text = elem.val().toLowerCase();
+  var src_text  = $('#regex-src').val();
+  var dest_text = $('#regex-dest').val();
+  var src_regex = new RegExp(src_text,'g');
+  elem.val(text.replace(src_regex,dest_text))
+}
+
+
+
+
